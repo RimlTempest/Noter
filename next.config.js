5e509d094408cd58fb11c4ee47e6c5fcf253dfb1
RimlTempest <riml.slime@gmail.com>
2021-07-23 01:37:37 +0900

    fix: eslint

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
-const withPWA = require("next-pwa");
+const withPWA = require('next-pwa');
 
 module.exports = {
     reactStrictMode: true,
+    eslint: {
+        dirs: ['src/pages/', 'src/components/', 'src/layout/', 'src/types/'],
+    },
 };
 
 module.exports = withPWA({
     pwa: {
-        dest: "public", // swの出力ディレクトリ
+        dest: 'public', // swの出力ディレクトリ
         // runtimeCaching: []
     },
 });
