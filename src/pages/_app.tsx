9463267dff5aeae901656e5ff0dd0de17d334f39
RimlTempest <riml.slime@gmail.com>
2021-07-23 02:58:25 +0900

    add: GA対応

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,24 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import { GA_TRACKING_ID, pageview } from '../lib/gtag';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+    const router = useRouter();
+    useEffect(() => {
+        // GA_TRACKING_ID が設定されていない場合は、処理終了
+        if (!GA_TRACKING_ID) return;
+
+        const handleRouteChange = (url: string) => {
+            pageview(url);
+        };
+        router.events.on('routeChangeComplete', handleRouteChange);
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+        };
+    }, [router.events]);
     return (
         <ChakraProvider>
             <Head>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ import Document, {
     DocumentContext,
     DocumentInitialProps,
 } from 'next/document';
+import { GA_TRACKING_ID } from '../lib/gtag';
 
 class MyDocument extends Document {
     static async getInitialProps(
@@ -18,6 +19,26 @@ class MyDocument extends Document {
         return (
             <Html lang="ja-JP" dir="ltr">
                 <Head>
+                    {GA_TRACKING_ID && (
+                        <>
+                            <script
+                                async
+                                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+                            />
+                            <script
+                                dangerouslySetInnerHTML={{
+                                    __html: `
+                                        window.dataLayer = window.dataLayer || [];
+                                        function gtag(){dataLayer.push(arguments);}
+                                        gtag('js', new Date());
+                                        gtag('config', '${GA_TRACKING_ID}', {
+                                        page_path: window.location.pathname,
+                                        });
+                                    `,
+                                }}
+                            />
+                        </>
+                    )}
                     {/* windows */}
                     <meta
                         name="msapplication-square70x70logo"
